fb94d10d68d628de505d71eda58037577327d339
kevinhaube <khaube@ametrics.org>
2021-06-29 19:05:26 -0400

    add button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppbarComponent } from './appbar/appbar.component';
 import { CountBoxComponent } from './count-box/count-box.component';
+import { AddButtonComponent } from './add-button/add-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppbarComponent,
-    CountBoxComponent
+    CountBoxComponent,
+    AddButtonComponent
   ],
   imports: [
     BrowserModule
